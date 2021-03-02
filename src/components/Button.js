527d1c16bf22287c1c2b36a5275be75a728f233b
lhenriquedev <lhenrique.dev@gmail.com>
2021-03-02 15:28:45 -0300

    first commit :rocket:

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Button = ({ color, text, onClick }) => {
+  return (
+    <div>
+      <button
+        style={{ backgroundColor: color }}
+        className='btn'
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+};
